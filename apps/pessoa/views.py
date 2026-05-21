39a82152cc15e88b7f162d1733a48593f499d70f
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-21 18:51:42 -0300

    fix: usa constante HTTP_200_OK no retorno da view

--- apps/pessoa/views.py
+++ apps/pessoa/views.py
@@ -1,11 +1,13 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class PessoaInfoView(APIView):
     def get(self, request):
-        return Response({
+        data = {
             'classe': 'Pessoa',
             'tipo': 'abstract',
             'atributos': ['nome', 'cpf', 'dataNascimento'],
-        })
+        }
+        return Response(data, status=status.HTTP_200_OK)
