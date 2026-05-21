afc8f1a72fe285b95081ae4291c7ddba7c5fb65c
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-21 18:34:09 -0300

    feat: cria view de info da Pessoa

--- apps/pessoa/views.py
+++ apps/pessoa/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class PessoaInfoView(APIView):
+    def get(self, request):
+        return Response({
+            'classe': 'Pessoa',
+            'tipo': 'abstract',
+            'atributos': ['nome', 'cpf', 'dataNascimento'],
+        })
