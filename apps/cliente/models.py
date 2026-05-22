540a55b8b4120a04f144d85e48384f411b5f8b05
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-22 14:32:51 -0300

    feat: implementa model Cliente

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from apps.pessoa.models import Pessoa
+
+
+class Cliente(Pessoa):
+    ativo = models.BooleanField(default=True)
+
+    def verificarElegibilidadeLocacao(self):
+        return self.ativo
+
+    def __str__(self):
+        return self.nome
