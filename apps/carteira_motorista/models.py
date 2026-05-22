7d724db0c5ab7b20403342f773aed4ba2bc63187
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-22 19:22:47 -0300

    feat: implementa model CarteiraMotorista

--- apps/carteira_motorista/models.py
+++ apps/carteira_motorista/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from apps.cliente.models import Cliente
+
+
+class CarteiraMotorista(models.Model):
+    numeroRegistro = models.CharField(max_length=20, unique=True)
+    categoria = models.CharField(max_length=5)
+    dataValidade = models.DateField()
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE, related_name='carteiraMotorista')
+
+    def isValida(self):
+        return self.dataValidade >= timezone.now().date()
+
+    def __str__(self):
+        return f"CNH {self.numeroRegistro} ({self.categoria})"
