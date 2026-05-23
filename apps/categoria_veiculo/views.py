d5b9c38a273f91f801b340e4870ada54b796eea4
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 18:42:17 -0300

    feat: cria CategoriaVeiculoViewSet

--- apps/categoria_veiculo/views.py
+++ apps/categoria_veiculo/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import CategoriaVeiculo
+from .serializer import CategoriaVeiculoSerializer
+
+
+class CategoriaVeiculoViewSet(viewsets.ModelViewSet):
+    queryset = CategoriaVeiculo.objects.all()
+    serializer_class = CategoriaVeiculoSerializer
