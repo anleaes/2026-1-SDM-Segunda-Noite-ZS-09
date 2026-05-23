fecc74e0dff2c5c33e3f743d7bb667b1c1ac30bd
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 15:08:42 -0300

    feat: cria AgenciaSerializer

---
+++ apps/agencia/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Agencia
+
+
+class AgenciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agencia
+        fields = ['id', 'nome', 'endereco', 'telefone', 'ativa']
