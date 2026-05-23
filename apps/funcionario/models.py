4b0b5d745622d7458ccf489970d270f30c6fbac0
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 11:22:47 -0300

    feat: implementa model Funcionario

--- apps/funcionario/models.py
+++ apps/funcionario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from apps.pessoa.models import Pessoa
+
+
+class Funcionario(Pessoa):
+    matricula = models.CharField(max_length=20, unique=True)
+    cargo = models.CharField(max_length=60)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    dataContratacao = models.DateField()
+
+    def __str__(self):
+        return f"{self.matricula} - {self.nome}"
