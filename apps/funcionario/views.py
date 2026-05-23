5a935fdff820b0cc659714ca52ac78cdd80fac6b
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 12:34:51 -0300

    feat: cria FuncionarioViewSet

--- apps/funcionario/views.py
+++ apps/funcionario/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Funcionario
+from .serializer import FuncionarioSerializer
+
+
+class FuncionarioViewSet(viewsets.ModelViewSet):
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer
