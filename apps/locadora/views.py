ddf0a7a1a026d54f21fe317cc388223261967128
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 15:00:00 -0300

    Add PessoaViewSet informational endpoint

--- apps/locadora/views.py
+++ apps/locadora/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class PessoaViewSet(APIView):
+    def get(self, request):
+        return Response(
+            {'detail': 'Pessoa é uma classe abstrata. Use /api/clientes/ ou /api/funcionarios/.'},
+            status=status.HTTP_200_OK,
+        )
