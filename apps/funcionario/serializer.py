6ba9c7ad949b30ae8db5989dd7e11c54bcce9010
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 11:48:14 -0300

    feat: cria FuncionarioSerializer

---
+++ apps/funcionario/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Funcionario
+
+
+class FuncionarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Funcionario
+        fields = ['id', 'nome', 'cpf', 'dataNascimento', 'matricula', 'cargo', 'salario', 'dataContratacao']
