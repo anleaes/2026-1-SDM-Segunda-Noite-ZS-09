9231ddea825329a8d396e8589359b4687a769ffe
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-22 22:30:00 -0300

    Add PessoaSerializer base class

---
+++ apps/locadora/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Pessoa
+
+
+class PessoaSerializer(serializers.Serializer):
+    nome = serializers.CharField(max_length=100)
+    cpf = serializers.CharField(max_length=14)
+    data_nascimento = serializers.DateField()
+    email = serializers.EmailField(required=False, default='')
