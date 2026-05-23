876e43e1836b86ab5b700e5e96ca3fccb44841a7
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 17:28:54 -0300

    feat: implementa model CategoriaVeiculo

--- apps/categoria_veiculo/models.py
+++ apps/categoria_veiculo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CategoriaVeiculo(models.Model):
+    nome = models.CharField(max_length=60)
+    descricao = models.TextField(blank=True)
+    capacidadePassageiros = models.PositiveIntegerField()
+    valorAdicional = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.nome
