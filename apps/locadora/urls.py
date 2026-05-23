5db715f771f81e8c080b9cdd91bef086372c648c
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 15:26:18 -0300

    Register CategoriaVeiculo route

--- apps/locadora/urls.py
+++ apps/locadora/urls.py
@@ -1,11 +1,18 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import AgenciaViewSet, CarteiraMotoristaViewSet, ClienteViewSet, FuncionarioViewSet
+from .views import (
+    AgenciaViewSet,
+    CarteiraMotoristaViewSet,
+    CategoriaVeiculoViewSet,
+    ClienteViewSet,
+    FuncionarioViewSet,
+)
 
 router = DefaultRouter()
 router.register(r'clientes', ClienteViewSet)
 router.register(r'carteiras-motorista', CarteiraMotoristaViewSet)
 router.register(r'funcionarios', FuncionarioViewSet)
 router.register(r'agencias', AgenciaViewSet)
+router.register(r'categorias-veiculo', CategoriaVeiculoViewSet)
 
 urlpatterns = router.urls
