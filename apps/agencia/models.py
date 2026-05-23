0f6f04853f99fba8f74c12900e36f628ca584b7a
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-23 14:32:18 -0300

    feat: implementa model Agencia e relaciona com Funcionario

--- apps/agencia/models.py
+++ apps/agencia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Agencia(models.Model):
+    nome = models.CharField(max_length=120)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    ativa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
