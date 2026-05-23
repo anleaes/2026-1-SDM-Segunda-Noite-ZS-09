26ea1a36cf38541d9321d7e48bd21354c07fbef4
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-22 22:30:00 -0300

    Add abstract Pessoa model

--- apps/locadora/models.py
+++ apps/locadora/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=14, unique=True)
+    data_nascimento = models.DateField('Data de Nascimento')
+    email = models.EmailField('E-mail', blank=True, default='')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.nome
