4dead74bd6854a55dd942ae1d0a155eb93f5ebc4
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-24 10:24:43 -0300

    feat: implementa model Veiculo

--- apps/veiculo/models.py
+++ apps/veiculo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from apps.agencia.models import Agencia
+from apps.categoria_veiculo.models import CategoriaVeiculo
+
+
+class Veiculo(models.Model):
+    placa = models.CharField(max_length=10, unique=True)
+    modelo = models.CharField(max_length=80)
+    ano = models.PositiveIntegerField()
+    valorDiaria = models.DecimalField(max_digits=10, decimal_places=2)
+    ipvaPago = models.BooleanField(default=False)
+    disponivel = models.BooleanField(default=True)
+    agencia = models.ForeignKey(Agencia, on_delete=models.PROTECT, related_name='veiculos')
+    categoria = models.ForeignKey(CategoriaVeiculo, on_delete=models.PROTECT, related_name='veiculos')
+
+    def atualizarDisponibilidade(self, disponivel):
+        self.disponivel = disponivel
+        self.save(update_fields=['disponivel'])
+
+    def __str__(self):
+        return f"{self.modelo} ({self.placa})"
