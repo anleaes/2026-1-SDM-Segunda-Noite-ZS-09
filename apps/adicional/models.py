f6fbccbfa75919051e5f63898c595c399b8bbb74
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-24 14:06:45 -0300

    feat: implementa model Adicional

--- apps/adicional/models.py
+++ apps/adicional/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Adicional(models.Model):
+    descricao = models.CharField(max_length=200)
+    valorDiaria = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.descricao
