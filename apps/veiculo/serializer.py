ccd4525ffe54db7a91458abf9192d0ed61c26f3c
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-24 11:08:32 -0300

    feat: cria VeiculoSerializer

---
+++ apps/veiculo/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Veiculo
+
+
+class VeiculoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ['id', 'placa', 'modelo', 'ano', 'valorDiaria', 'ipvaPago', 'disponivel', 'agencia', 'categoria']
