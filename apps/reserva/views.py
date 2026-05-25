28903a9b227e2b62f812098bc510399f91503720
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 16:24:02 -0300

    feat: cria ReservaViewSet

--- apps/reserva/views.py
+++ apps/reserva/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Reserva
+from .serializer import ReservaSerializer
+
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
