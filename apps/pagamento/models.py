83740108e858d198888666b05a5bdf3c748f7716
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 16:58:43 -0300

    feat: implementa model Pagamento

--- apps/pagamento/models.py
+++ apps/pagamento/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from apps.reserva.models import Reserva
+
+
+class Pagamento(models.Model):
+    dataPagamento = models.DateField(null=True, blank=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    metodoPagamento = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, default='PENDENTE')
+    reserva = models.OneToOneField(Reserva, on_delete=models.CASCADE, related_name='pagamento')
+
+    def processarPagamento(self):
+        self.status = 'PAGO'
+        self.dataPagamento = timezone.now().date()
+        self.save(update_fields=['status', 'dataPagamento'])
+        return True
+
+    def estornarPagamento(self):
+        self.status = 'ESTORNADO'
+        self.save(update_fields=['status'])
+        return True
+
+    def __str__(self):
+        return f"Pagamento {self.id} - Reserva {self.reserva.id}"
