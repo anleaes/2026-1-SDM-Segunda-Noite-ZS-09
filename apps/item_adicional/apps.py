643ac0b9091113927c603944d7d74db0d54b1f89
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 19:02:44 -0300

    feat: cria app item_adicional

---
+++ apps/item_adicional/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ItemAdicionalConfig(AppConfig):
+    name = 'apps.item_adicional'
+
--- locadoraApp/settings.py
+++ locadoraApp/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'apps.seguro',
     'apps.reserva',
     'apps.pagamento',
+    'apps.item_adicional',
 ]
+
 
 
 
