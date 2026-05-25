f4be3cd168bc0f9eaf8c3d65e43f31d8deb56776
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 19:24:19 -0300

    feat: cria ItemAdicionalViewSet

--- apps/item_adicional/views.py
+++ apps/item_adicional/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import ItemAdicional
+from .serializer import ItemAdicionalSerializer
+
+
+class ItemAdicionalViewSet(viewsets.ModelViewSet):
+    queryset = ItemAdicional.objects.all()
+    serializer_class = ItemAdicionalSerializer
