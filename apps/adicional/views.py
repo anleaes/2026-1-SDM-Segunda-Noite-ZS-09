2207081bb53cebbeda299919141af80ccfea84a2
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 07:11:29 -0300

    Add AdicionalViewSet

--- apps/adicional/views.py
+++ apps/adicional/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Adicional
+from .serializer import AdicionalSerializer
+
+
+class AdicionalViewSet(viewsets.ModelViewSet):
+    queryset = Adicional.objects.all().order_by('id')
+    serializer_class = AdicionalSerializer
