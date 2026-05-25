605ebe262162e924beaa2ef21528001a9973df2b
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 20:58:11 -0300

    feat: cria MultaViewSet

--- apps/multa/views.py
+++ apps/multa/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Multa
+from .serializer import MultaSerializer
+
+
+class MultaViewSet(viewsets.ModelViewSet):
+    queryset = Multa.objects.all()
+    serializer_class = MultaSerializer
