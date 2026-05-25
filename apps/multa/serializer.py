f077575a60f3954cd213e570685f25796f938d97
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 20:48:19 -0300

    feat: cria MultaSerializer

---
+++ apps/multa/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Multa
+
+
+class MultaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Multa
+        fields = ['id', 'valor', 'descricao', 'dataInfracao', 'pago', 'reserva']
