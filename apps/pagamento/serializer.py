d89d5140dd0e5f2d8749c8e85123c0d320630c98
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 17:09:05 -0300

    feat: marca id como read-only no PagamentoSerializer

--- apps/pagamento/serializer.py
+++ apps/pagamento/serializer.py
@@ -7,3 +7,5 @@ class PagamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pagamento
         fields = ['id', 'dataPagamento', 'valor', 'metodoPagamento', 'status', 'reserva']
+        read_only_fields = ['id']
+
