4822babe8989c1ced0b2b3c09d02cc28236e8b10
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 14:42:11 -0300

    feat: implementa model Seguro

--- apps/seguro/models.py
+++ apps/seguro/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Seguro(models.Model):
+    franquia = models.DecimalField(max_digits=10, decimal_places=2)
+    valorDiaria = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.descricao
