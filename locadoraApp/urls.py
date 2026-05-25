b781e88302f2a731ce5aba14ecebe8128ad11d21
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-25 20:22:18 -0300

    feat: registra urls de Manutencao no projeto

--- locadoraApp/urls.py
+++ locadoraApp/urls.py
@@ -15,7 +15,9 @@ urlpatterns = [
     path('api/', include('apps.reserva.urls')),
     path('api/', include('apps.pagamento.urls')),
     path('api/', include('apps.item_adicional.urls')),
+    path('api/', include('apps.manutencao.urls')),
 ]
+
 
 
 
