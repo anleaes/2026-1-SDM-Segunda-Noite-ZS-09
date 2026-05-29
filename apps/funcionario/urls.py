80515dddb522a65ae21e44749cb2251253598396
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-29 13:18:53 -0300

    refact: reestruturando views e urls de funcionario

--- apps/funcionario/urls.py
+++ apps/funcionario/urls.py
@@ -1,8 +1,8 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from . import views
 
-from .views import FuncionarioViewSet
-
-router = DefaultRouter()
-router.register(r'funcionarios', FuncionarioViewSet)
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('lista/', views.funcionario_list, name='funcionario_list'),
+    path('criar/', views.funcionario_create, name='funcionario_create'),
+    path('deletar/<int:id>/', views.funcionario_delete, name='funcionario_delete'),
+]
--- apps/funcionario/views.py
+++ apps/funcionario/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
+from django.shortcuts import render, redirect, get_object_or_404
 
+from apps.agencia.models import Agencia
 from .models import Funcionario
 from .serializer import FuncionarioSerializer
 
@@ -7,3 +9,48 @@ from .serializer import FuncionarioSerializer
 class FuncionarioViewSet(viewsets.ModelViewSet):
     queryset = Funcionario.objects.all().order_by('matricula')
     serializer_class = FuncionarioSerializer
+
+
+def funcionario_list(request):
+    pagina = 'funcionario/list_funcionario.html'
+    todos_funcionarios = Funcionario.objects.all().order_by('id')
+    todas_agencias = Agencia.objects.all().order_by('id')
+    dados_para_tela = {
+        'funcionarios': todos_funcionarios,
+        'agencias': todas_agencias
+    }
+    return render(request, pagina, dados_para_tela)
+
+
+def funcionario_create(request):
+    if request.method == 'POST':
+        nome_digitado = request.POST['nome']
+        cpf_digitado = request.POST['cpf']
+        data_nascimento_digitada = request.POST['dataNascimento']
+        matricula_digitada = request.POST['matricula']
+        cargo_digitado = request.POST['cargo']
+        salario_digitado = request.POST['salario']
+        data_contratacao_digitada = request.POST['dataContratacao']
+        id_da_agencia = request.POST.get('agencia_id')
+
+        agencia_escolhida = None
+        if id_da_agencia:
+            agencia_escolhida = get_object_or_404(Agencia, id=id_da_agencia)
+
+        Funcionario.objects.create(
+            nome=nome_digitado,
+            cpf=cpf_digitado,
+            dataNascimento=data_nascimento_digitada,
+            matricula=matricula_digitada,
+            cargo=cargo_digitado,
+            salario=salario_digitado,
+            dataContratacao=data_contratacao_digitada,
+            agencia=agencia_escolhida
+        )
+    return redirect('funcionario_list')
+
+
+def funcionario_delete(request, id):
+    funcionario_para_deletar = get_object_or_404(Funcionario, id=id)
+    funcionario_para_deletar.delete()
+    return redirect('funcionario_list')
