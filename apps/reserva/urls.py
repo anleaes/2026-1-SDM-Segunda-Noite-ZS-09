e02cb51245f911d6086ee6bad5c059dddac96418
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-29 13:33:08 -0300

    refact: reestruturando views e urls de reserva

--- apps/reserva/urls.py
+++ apps/reserva/urls.py
@@ -1,8 +1,8 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from . import views
 
-from .views import ReservaViewSet
-
-router = DefaultRouter()
-router.register(r'reservas', ReservaViewSet)
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('lista/', views.reserva_list, name='reserva_list'),
+    path('criar/', views.reserva_create, name='reserva_create'),
+    path('deletar/<int:id>/', views.reserva_delete, name='reserva_delete'),
+]
