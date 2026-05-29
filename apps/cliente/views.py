137d160926dc6cf6dce937b9fbe38e145a4811f6
Thiago Roncoli <thiago.roncolibrusch@gmail.com>
2026-05-29 10:57:00 -0300

    refact: reestruturando views e urls de cliente

--- apps/cliente/views.py
+++ apps/cliente/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Cliente
 from .serializer import ClienteSerializer
@@ -7,3 +8,34 @@ from .serializer import ClienteSerializer
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all().order_by('id')
     serializer_class = ClienteSerializer
+
+
+def cliente_list(request):
+    pagina = 'cliente/list_cliente.html'
+    todos_clientes = Cliente.objects.all().order_by('id')
+    dados_para_tela = {
+        'clientes': todos_clientes
+    }
+    return render(request, pagina, dados_para_tela)
+
+
+def cliente_create(request):
+    if request.method == 'POST':
+        nome_digitado = request.POST['nome']
+        cpf_digitado = request.POST['cpf']
+        data_digitada = request.POST['dataNascimento']
+        esta_ativo = 'ativo' in request.POST
+
+        Cliente.objects.create(
+            nome=nome_digitado,
+            cpf=cpf_digitado,
+            dataNascimento=data_digitada,
+            ativo=esta_ativo
+        )
+    return redirect('cliente_list')
+
+
+def cliente_delete(request, id):
+    cliente_para_deletar = get_object_or_404(Cliente, id=id)
+    cliente_para_deletar.delete()
+    return redirect('cliente_list')
