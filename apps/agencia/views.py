5f5ed4b7e27846bc09ce581bb906b3f9d1dd7821
Marcelo Duarte de Aguiar <celo.duarte.2005@gmail.com>
2026-05-29 12:34:17 -0300

    refact: reestruturando views e urls de agencia

--- apps/agencia/views.py
+++ apps/agencia/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Agencia
 from .serializer import AgenciaSerializer
@@ -7,3 +8,34 @@ from .serializer import AgenciaSerializer
 class AgenciaViewSet(viewsets.ModelViewSet):
     queryset = Agencia.objects.all().order_by('nome')
     serializer_class = AgenciaSerializer
+
+
+def agencia_list(request):
+    pagina = 'agencia/list_agencia.html'
+    todas_agencias = Agencia.objects.all().order_by('id')
+    dados_para_tela = {
+        'agencias': todas_agencias
+    }
+    return render(request, pagina, dados_para_tela)
+
+
+def agencia_create(request):
+    if request.method == 'POST':
+        nome_digitado = request.POST['nome']
+        endereco_digitado = request.POST['endereco']
+        telefone_digitado = request.POST['telefone']
+        esta_ativa = 'ativa' in request.POST
+
+        Agencia.objects.create(
+            nome=nome_digitado,
+            endereco=endereco_digitado,
+            telefone=telefone_digitado,
+            ativa=esta_ativa
+        )
+    return redirect('agencia_list')
+
+
+def agencia_delete(request, id):
+    agencia_para_deletar = get_object_or_404(Agencia, id=id)
+    agencia_para_deletar.delete()
+    return redirect('agencia_list')
